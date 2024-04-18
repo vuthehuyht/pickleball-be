4ebd0f04c6317f8d8e3190019effda34577ee2fb
Vu The Huy <vuthehuy.t61@gmail.com>
2024-04-18 21:04:20 +0700

    add missing files

---
+++ src/main/java/com/macrace/pickleball/PickleBallApplication.java
@@ -0,0 +1,13 @@
+package com.macrace.pickleball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickleBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickleBallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macrace/pickleball/PickleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macrace.pickleball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

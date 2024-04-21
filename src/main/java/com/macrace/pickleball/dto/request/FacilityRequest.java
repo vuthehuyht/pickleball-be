2598c9081da99605838a54090bccdcbb94dc39b4
Vu The Huy <vuthehuy.t61@gmail.com>
2024-04-21 18:31:32 +0700

    refactor: update facility model and add more testcases

--- src/main/java/com/macrace/pickleball/dto/request/FacilityRequest.java
+++ src/main/java/com/macrace/pickleball/dto/request/FacilityRequest.java
@@ -1,5 +1,6 @@
 package com.macrace.pickleball.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,4 +8,11 @@ import lombok.Data;
 public class FacilityRequest {
     @NotBlank(message = "Facility name is required")
     private String name;
+
+    @NotBlank(message = "Facility address is required")
+    private String address;
+
+    @NotBlank(message = "Phone number is required")
+    @JsonProperty("phone_number")
+    private String phoneNumber;
 }
--- src/main/java/com/macrace/pickleball/model/Facility.java
+++ src/main/java/com/macrace/pickleball/model/Facility.java
@@ -2,6 +2,7 @@ package com.macrace.pickleball.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.geo.Point;
 
 @Getter
 @Setter
@@ -17,6 +18,15 @@ public class Facility {
 
     @Column(name = "name")
     private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "location")
+    private Point location;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
